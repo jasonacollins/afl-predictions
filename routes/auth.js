9fe4c625a09babcaf336f6afb245cf396a8029c2
Jason Collins <18749854+jasonacollins@users.noreply.github.com>
2025-04-21 10:04:34 +1000

    update rate limit

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 attempts per window
+  max: 20, // 5 attempts per window
   message: 'Too many login attempts, please try again later',
   standardHeaders: true,
   legacyHeaders: false,
